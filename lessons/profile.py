3bba117c21662b30428159b96f3e08df426bd122
Yan Zhu <zhuyan@unc.edu>
2022-11-01 20:54:05 -0400

    class

--- lessons/profile.py
+++ lessons/profile.py
@@ -1,25 +1,21 @@
 """Examples of a class and objects."""
 
-import profile
-
-
 class Profile:
     handle: str
     followers: int
     is_private: bool
-
-    def ___init___(self, handle: str):
-        """Constructornitializes attributes."""
+    
+    def __init__(self, handle: str):
+        """Constructor initializes attributes."""
         self.handle = handle
         self.followers = 0
         self.is_private = False
 
-    def tweet(self, msg: str):
+
+    def tweet(self, msg: str) -> None:
         """An example of a method."""
         print(f"@{self.handle} tweets {msg}")
 
-
+# method calls
 my_profile: Profile = Profile("krisjordan")
 my_profile.tweet("Hello, world.")
-
-
